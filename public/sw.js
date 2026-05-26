27f92329eac1ac341b3cbccf23596ec3fc0e8c01
judeumba745 <judeumba745@gmail.com>
2026-05-26 21:12:50 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,6 @@
-self.addEventlistener('install', () => {
-  self.skipWaiting();
+self.addEventlistener('install', (e) => {
+  console.log('service worker installé');
+});
+
+self.addEventlistener('fetch', (e) => {
 });
